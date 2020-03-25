1b3e0f56d2786842db2425ae7e88871fd110823e
miao-ba <bbcjkloil@gmail.com>
2020-03-25 19:04:34 +0800

    yup

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ def handle_message(event):
             ]
         )
     )
-line_bot_api.reply_message(event.reply_token, message)
+line_bot_api.reply_message(MessageEvent.reply_token,message)
 
 if __name__=="__main__":
     port=int(os.environ.get("port",5000))
