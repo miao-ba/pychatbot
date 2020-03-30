deff134160e999cb7d97a11a3c6a5ccce2a1c52e
miao-ba <bbcjkloil@gmail.com>
2020-03-30 20:34:06 +0800

    u

--- app.py
+++ app.py
@@ -192,6 +192,7 @@ def handle_message(event):
                 ]
             )
         )
+        return 0
     if event.message.text == "油價查詢":
         content = oil_price()
         line_bot_api.reply_message(
